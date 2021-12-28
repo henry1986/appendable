8f25dd3cb67efe70c736088ed70fe45414810235
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2021-12-28 21:10:24 +0100

    ..

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 buildscript {
     repositories {
         maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
-        maven("https://daiv.org/artifactory/gradle-dev-local")
+        maven("https://artifactory.daiv.org/artifactory/gradle-dev-local")
     }
     dependencies {
-        classpath("org.daiv.dependency:DependencyHandling:0.0.58")
+        classpath("org.daiv.dependency:DependencyHandling:0.0.96")
     }
 }
 
@@ -23,7 +23,8 @@ version = versions.setVersion { appendable }
 
 repositories {
     mavenCentral()
-    maven("https://daiv.org/artifactory/gradle-dev-local")
+    maven("https://dl.bintray.com/kotlin/kotlinx")
+    maven("https://artifactory.daiv.org/artifactory/gradle-dev-local")
 }
 kotlin {
     jvm {
@@ -41,18 +42,15 @@ kotlin {
             }
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
 
     
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting{
+            dependencies{
+                implementation(versions.kutil())
+                implementation(versions.kotlinx_html())
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -71,8 +69,6 @@ kotlin {
                 implementation(kotlin("test-js"))
             }
         }
-        val nativeMain by getting
-        val nativeTest by getting
     }
 }
 
--- src/jsMain/kotlin/org/daiv/appendable/Appendable.kt
+++ src/jsMain/kotlin/org/daiv/appendable/Appendable.kt
@@ -1,5 +1,11 @@
 package org.daiv.appendable
 
+import kotlinx.browser.document
+import kotlinx.html.DIV
+import kotlinx.html.classes
+import kotlinx.html.dom.create
+import kotlinx.html.js.div
+import mu.KotlinLogging
 import org.w3c.dom.Node
 
 interface Appendable {
@@ -7,6 +13,8 @@ interface Appendable {
     fun letAppend(node: Node) {
         node.appendChild(this.node)
     }
+
+    fun div(classes: String? = null, block: DIV.() -> Unit) = document.create.div(classes = classes, block = block)
 
     fun remove(node: Node) {
         if (this.node.parentNode == node) {
@@ -17,6 +25,56 @@ interface Appendable {
     }
 
     companion object {
-        private val logger = KotlinLogging.logger("org.daiv.ew.frontend.header.Appendable")
+        private val logger = KotlinLogging.logger("org.daiv.appendable.Appendable")
     }
+}
+
+interface SiteHolder<T : Appendable> {
+    fun setNewSubNode(appendable: T)
+    fun deleteLastNodes()
+    fun resetToLast()
+}
+
+interface SiteHolderExt<T : Appendable> : SiteHolder<T> {
+    var currentSubNode: T?
+    val node: Node
+
+    val lastNodes: MutableList<T>
+
+    override fun setNewSubNode(appendable: T) {
+        if (appendable != currentSubNode) {
+            currentSubNode?.let {
+                it.remove(node)
+                lastNodes.add(it)
+            }
+            appendable.letAppend(node)
+            currentSubNode = appendable
+        }
+    }
+
+    override fun resetToLast() {
+        if (lastNodes.isEmpty()) {
+            return
+        }
+        currentSubNode?.remove(node)
+        val last = lastNodes.last()
+        last.letAppend(node)
+        currentSubNode = last
+        lastNodes.removeAt(lastNodes.size - 1)
+    }
+
+    override fun deleteLastNodes() {
+        lastNodes.clear()
+    }
+}
+
+class SiteHolderImpl<T : Appendable>(override val node: Node) : SiteHolderExt<T>, Appendable {
+    constructor(css: String) : this(document.create.div { classes = setOf(css) })
+
+    override var currentSubNode: T? = null
+    override val lastNodes: MutableList<T> = mutableListOf()
+}
+
+interface SiteReseter : SiteHolder<Appendable> {
+    fun reset()
 }
